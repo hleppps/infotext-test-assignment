ead72f515ded2ef80e123eab3438f1777e9b7ce6
Hlieb Shchehelskyi <hlieb.shchehelskyi@vilmate.com>
2023-06-11 14:22:51 +0300

    feat: create file with dummy characters

---
+++ src/utils/dummyCharacters.ts
@@ -0,0 +1,8 @@
+import { Character } from '../../types/global';
+
+export const dummyCharacters: Character[] = [
+  { id: 1, name: 'Character 1', iconSrc: '../assets/images/character-1.png' },
+  { id: 2, name: 'Character 2', iconSrc: '../assets/images/character-2.png' },
+  { id: 3, name: 'Character 3', iconSrc: '../assets/images/character-3.png' },
+  { id: 4, name: 'Character 4', iconSrc: '../assets/images/character-4.png' },
+];
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,20 +1,5 @@
-import { GridStep } from 'utils/constants';
-
-export type Address = { lat: number; lng: number };
-
-export type Flat = {
-  id: string;
-  previewImage: string;
-  address: {
-    coordinates: {
-      lat: number;
-      lng: number;
-    };
-    street: string;
-    streetAddress: string;
-  };
-  rooms: number;
-  rentPrice: number;
+export type Character = {
+  id: number | string;
+  name: string;
+  iconSrc: string;
 };
-
-export type Tile = Address
