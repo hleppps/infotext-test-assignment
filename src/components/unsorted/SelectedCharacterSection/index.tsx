eb755a30b8e171b9807d5c2637a692e20f2fae57
Hlieb Shchehelskyi <hlieb.shchehelskyi@vilmate.com>
2023-06-12 14:14:27 +0300

    feat: updated images for not selected character

--- src/components/unsorted/SelectedCharacterSection/index.tsx
+++ src/components/unsorted/SelectedCharacterSection/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 
 import pedestalImage from '../../../assets/images/pedestal.svg';
+import pedestalActiveImage from '../../../assets/images/pedestal-active.svg';
 import styles from './styles.module.scss';
 
 export type SelectedCharacterSectionProps = {
@@ -12,21 +13,24 @@ export const SelectedCharacterSection: FC<SelectedCharacterSectionProps> = ({
   character,
   title,
 }) => {
-  const {name, imageSrc} = character || {}
+  const { name, imageSrc } = character || {};
   return (
     <div className={styles.section}>
       <div className={styles.header}>
         {title && <h3 className={styles.title}>{title}:</h3>}
         {name && <h4 className={styles.characterName}>({name})</h4>}
       </div>
-      <div className={styles.characterContainer}>
+      <div className={styles.content}>
+        {character && (
+          <div className={styles.characterContainer}>
+            <img className={styles.character} src={imageSrc} alt="Character" />
+          </div>
+        )}
         <img
-          className={styles.character}
-          src={imageSrc}
-          alt="Character"
+          src={character ? pedestalActiveImage : pedestalImage}
+          alt="Pedestal"
         />
       </div>
-      <img src={pedestalImage} alt="Pedestal" />
     </div>
   );
 };
